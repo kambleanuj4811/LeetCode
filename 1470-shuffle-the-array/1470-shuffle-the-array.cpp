c045afb69d3dd54c0ba6c9f469313321854c15d3
Anuj <kambleanuj4811@gmail.com>
2026-07-26 16:42:41 +0530

    Time: 8 ms (17.65%), Space: 13.5 MB (19.27%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int>ans;
+        int sz = nums.size();
+        int i = 0;
+        int j = n;
+        while(j<sz){
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+            i++;
+            j++;
+        }
+        return ans;
+        
+    }
+};
